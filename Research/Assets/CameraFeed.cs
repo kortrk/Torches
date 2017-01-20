27d1b363838ceee9a0bd5dfe6707c8bd70730815
sirkevin813 <krkortright@gmail.com>
2017-01-20 07:58:33 -0500

    First run of the webcam feed. Mac’s webcam is showing up in the game!

---
+++ Research/Assets/CameraFeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFeed : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		foreach (WebCamDevice w in WebCamTexture.devices) {
+			print (w.name);
+		}
+		WebCamTexture webcam = new WebCamTexture (WebCamTexture.devices [0].name);
+		GetComponent<MeshRenderer> ().material.mainTexture = webcam;
+		webcam.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
