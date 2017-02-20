b46ad0ca953b54cdfa0ed55afef8a2fce4bb2163
sirkevin813 <kortrk@rpi.edu>
2017-02-20 12:35:18 -0500

    Completed the BFS implementation. It doesn't get the whole paddle yet, so I have to either modify it, or try something new.

--- Research/Assets/CameraFeed.cs
+++ Research/Assets/CameraFeed.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Pixel{
 	Color color;
-	int x;
-	int y;
+	public int x;
+	public int y;
 
 	public Pixel(int x_, int y_){
 		x = x_;
 		y = y_;
+	}
+
+	public string toString(){
+		return x + ", " + y;
 	}
 }
 
@@ -20,20 +24,19 @@ public class CameraFeed : MonoBehaviour {
 
 	//public variables
 	public GameObject color_indicator;
+	public GameObject single_pixel;
 
 	//each paddle is a set of pixels
-	List<HashSet<Pixel>> green_paddles;
-	List<HashSet<Pixel>> red_paddles;
+	List<HashSet<int>> green_paddles;
+	List<HashSet<int>> red_paddles;
 
 	WebCamTexture webcam;
-
-	int g_paddles_found = 0;
 
 	// Use this for initialization
 	void Start () {
 		//set up the paddle lists
-		green_paddles = new List<HashSet<Pixel>> ();
-		red_paddles = new List<HashSet<Pixel>> ();
+		green_paddles = new List<HashSet<int>> ();
+		red_paddles = new List<HashSet<int>> ();
 
 		foreach (WebCamDevice w in WebCamTexture.devices) {
 			print (w.name);
@@ -45,7 +48,9 @@ public class CameraFeed : MonoBehaviour {
 			GetComponent<SpriteRenderer> ().material.mainTexture = webcam;
 		webcam.requestedWidth = WIDTH;
 		webcam.requestedHeight = HEIGHT;
+		webcam.requestedFPS = 60;
 		webcam.Play ();
+
 	}
 	
 	// Update is called once per frame
@@ -54,9 +59,11 @@ public class CameraFeed : MonoBehaviour {
 
 
 		//TEST
-		if (Input.GetKeyDown (KeyCode.P))
-			print (g_paddles_found);
-		mouseColorTest();
+		if (Input.GetKeyDown (KeyCode.P)) {
+			print (green_paddles.Count);
+			drawPaddlesToScreen ();
+		}
+		//mouseColorTest();
 	}
 		
 	Vector2 mouseInWorld(){
@@ -81,6 +88,9 @@ public class CameraFeed : MonoBehaviour {
 	}
 	
 	void getPaddles(){
+		//clear out the paddles already in there
+		green_paddles.Clear();
+		red_paddles.Clear ();
 		//Using Google Color Selector:
 		//google.com/#q=color+selector
 		//Green	hue is between 102 and 140
@@ -90,7 +100,7 @@ public class CameraFeed : MonoBehaviour {
 		//		sat > 55
 		//		val > 33
 		Color[] pixels = webcam.GetPixels();
-		HashSet<int> already_in_paddles;
+		HashSet<int> already_in_paddles = new HashSet<int>();
 		float h = 0;
 		float s = 0;
 		float v = 0;
@@ -98,14 +108,15 @@ public class CameraFeed : MonoBehaviour {
 			if (already_in_paddles.Contains (i)) continue;
 			Color.RGBToHSV (pixels [i], out h, out s, out v);
 			if (isGreen(h,s,v)) {
-				HashSet<Pixel> paddle = BFS (i, pixels, already_in_paddles, "green");
+				HashSet<int> paddle = BFS (i, pixels, already_in_paddles, "green");
 				//we may actually want a paddle object: {pxl_set, center}
 				green_paddles.Add (paddle);
+				already_in_paddles.UnionWith (paddle);
 			}
 		}
 	}
 
-	HashSet<int> BFS (int i, Color[] pixels, HashSet<Pixel> already_in_pixels, string color){
+	HashSet<int> BFS (int i, Color[] pixels, HashSet<int> already_in_paddles, string color){
 		//add this to a paddle hashset
 		HashSet<int> paddle = new HashSet<int>();
 		HashSet<int> visited = new HashSet<int>();	//all the pixels we've seen, including non-red/non-green
@@ -115,54 +126,84 @@ public class CameraFeed : MonoBehaviour {
 
 		while (q.Count != 0) {
 			int x = q.Dequeue ();
+			paddle.Add (x);
 			//there are potentially eight neighbors for this pixel
-			List<int> neighbors = getPixelNeighbors(i, pixels.Length);
+			List<int> neighbors = getPixelNeighbors(x, pixels.Length);
 			//check each neighbor - if it's the right color, it goes in q
-			Vector3 i_color = hsvAt(i, pixels); //pun
-			if (color=="green" && isGreen(i_color.x,i_color.y,i_color.z) /*|| (color=="red"&& isRed())*/){
-
+			for (int y = 0; y < neighbors.Count; y++) {
+				if (!visited.Contains (neighbors[y])) 
+					visited.Add (neighbors[y]);
+				else 
+					continue;
+				Vector3 y_color = hsvAt (neighbors[y], pixels);
+				if (color == "green" && isGreen (y_color.x, y_color.y, y_color.z) /*|| (color=="red"&& isRed())*/) {
+					q.Enqueue (neighbors[y]);
+				}
 			}
 		}
+			
+		return paddle;
 
 	}
 
 	Vector3 hsvAt(int i, Color[] pixels){
-		//TODO: get the hsv of an index in pixels
-		return new Vector3 ();
+		//get the hsv of an index in pixels
+		float h,s,v;
+		Color.RGBToHSV (pixels [i], out h, out s, out v);
+		return new Vector3 (h, s, v);
 	}
 
 	List<int> getPixelNeighbors(int i, int length){
 		List<int> result = new List<int> ();
 		if (i + 1 < length) //right
 			result.Add (i + 1);
-		if (i - 1 > 0) //left
+		if (i - 1 >= 0) //left
 			result.Add (i - 1);
-		if (i - WIDTH > 0)//above
+		if (i - WIDTH >= 0)//above
 			result.Add(i - WIDTH);
 		if (i + WIDTH < length)//below
 			result.Add(i + WIDTH);
-		if (i - WIDTH + 1 > 0)//diag r above
+		if (i - WIDTH + 1 >= 0)//diag r above
 			result.Add(i - WIDTH + 1);
-		if (i - WIDTH - 1 > 0)//diag l above
+		if (i - WIDTH - 1 >= 0)//diag l above
 			result.Add(i - WIDTH - 1);
 		if (i + WIDTH + 1 < length)//diag r below
 			result.Add(i + WIDTH + 1);
 		if (i + WIDTH - 1 < length)//diag l below
-			result.Add(i - WIDTH + 1);
+			result.Add(i + WIDTH - 1);
 		return result;
 	}
 
 	bool isGreen(float h, float s, float v){
 		//the range of valid green pixels:
-		return 160 < h * 360 && h * 360 < 170 && s * 100 > 50 && v * 100 > 33;
+		return 160 < h * 360 && h * 360 < 170 && s * 100 > 50 /*&& v * 100 > 33*/;
 	}
 
-	Pixel xyPixel(int i, int pixels_len){
+	Pixel get_Pixel_xy(int i, int pixels_len){
 		//makes a pixel given its 
 		//index in pixels array
-		int y_val = i / HEIGHT;
-		int x_val = pixels_len - y_val;
+		int y_val = i / WIDTH;
+		int x_val = i - WIDTH*y_val;
 		return new Pixel (x_val, y_val);
+	}
+
+	void drawPaddlesToScreen(){
+		GameObject container;
+		container = GameObject.Find ("Pixels");
+		if (container) {
+			Destroy (container);
+		}
+		container = new GameObject ();
+		container.name = "Pixels";
+		for (int x = 0; x < green_paddles.Count; x++) {
+			Color c = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+			foreach (int i  in green_paddles[x]) {
+				Pixel p = get_Pixel_xy (i, WIDTH * HEIGHT);
+				GameObject pixel = (GameObject) Instantiate (single_pixel, new Vector3 (p.x/10f, p.y/10f, 0), Quaternion.identity);
+				pixel.GetComponent<SpriteRenderer> ().color = c;
+				pixel.transform.parent = container.transform;
+			}
+		}
 	}
 
 	/*
